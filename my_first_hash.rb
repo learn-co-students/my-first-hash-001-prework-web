705a24c530bd9e6e077058dfe85d219fd622281f
SharkBroPhD <troychryssos@gmail.com>
2016-07-11 13:31:44 -0400

    Wrote some methods and hashes to help our boy Herm do his dang job.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,9 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = { "Theo" => "Brother",
+    "Sam" => "Poil",
+    "Adam" => "Huey"
+  }
 
-  
 end
 
 
@@ -11,20 +13,22 @@ def shipping_manifest
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
 
+shipping_manifest= { "whale bone corsets" => 5,
+  "porcelain vases" => 2,
+  "oil paintings" => 3
+}
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   
-  #your code here
-  #remember to return the shipping_manifest hash
-
+  shipping_manifest["pearl necklace"]=1
+  shipping_manifest
 end
 
 
